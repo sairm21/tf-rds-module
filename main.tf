65e64e31431399a9a12c716cca1fadef5efb60ae
sairam <sairm21@gmail.com>
2023-10-11 21:48:55 +0530

    correcting name of aws db subnet group

--- main.tf
+++ main.tf
@@ -16,5 +16,6 @@ resource "aws_rds_cluster" "shipping" {
   master_username         = data.aws_ssm_parameter.mysql_username.value
   master_password         = data.aws_ssm_parameter.mysql_password.value
   db_subnet_group_name = aws_db_subnet_group.mysql_cluster_subent_group.name
+  storage_encrypted = true
   kms_key_id = var.kms_key_id
 }
